20a9f22807de8f8228adee6363d9bf943c958346
wyxdlz54188 <liyuqili_1379zi@qq.com>
2024-12-29 12:58:02 +0000

    updata

---
+++ public/config.php
@@ -0,0 +1,11 @@
+<?php
+/*数据库配置*/
+$dbconfig=array(
+	"host" => "localhost", //数据库服务器
+	"port" => 3306, //数据库端口
+	"user" => "test123", //数据库用户名
+	"dbname" => "test123", //数据库名
+	"pwd" => "e4T7CPNE8Am7XCTE", //数据库密码
+	"dbqz" => "shua" //数据表前缀
+);
+?>
